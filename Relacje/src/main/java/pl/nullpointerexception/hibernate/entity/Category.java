09b02559478b9c65b06ad360dad5396fb4ce006b
Macstenc <118284307+Macstenc@users.noreply.github.com>
2022-11-29 12:32:48 +0100

    Add files via upload

---
+++ Relacje/src/main/java/pl/nullpointerexception/hibernate/entity/Category.java
@@ -0,0 +1,53 @@
+package pl.nullpointerexception.hibernate.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "category")
+    private List<Product> product;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Product> getProduct() {
+        return product;
+    }
+
+    public void setProduct(List<Product> product) {
+        this.product = product;
+    }
+}
